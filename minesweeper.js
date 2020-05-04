9c3df1a93cea6f9271363938fdda798f029aaa6d
Joe Butler <joefish.b@gmail.com>
2020-05-05 09:04:27 +1200

    Add function to check for win scenario

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
+// Create the board
 let board = {
   cells: [
     {
@@ -11,7 +12,7 @@ let board = {
     {
       row: 0,
       col: 1,
-      isMine: false,
+      isMine: true,
       hidden: true
     },
     {
@@ -58,51 +59,58 @@ let board = {
     }]
 }
 
+// Function to set up the game
 function startGame () {
-  for (i = 0; i <= board.cells.length - 1; i++) {
+
+  // For each cell, determine the number of surrounding cells that are mines to display as hints
+  for (i = 0; i < board.cells.length; i++) {
     let cell = board.cells[i];
     cell.surroundingMines = countSurroundingMines(cell);
   }
 
-  //document.addEventListener("click", checkForWin);
-  //document.addEventListener("contextmenu", checkForWin);
+  // Events to check for a win scenario
+  document.addEventListener("click", checkForWin);
+  document.addEventListener("contextmenu", checkForWin);
 
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
 
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
-/*function checkForWin () {
-  for (i = 0; i <= board.cells.length - 1; i++) {
-    let cell = board.cells[i]
-    if (cell.isMine == true && cell.isMarked == true) {
-      displayMessage();
-    } else {
-      return
+/* Function to check for a win condition:
+1. All cells that are not mines are visible, or;
+2. All mines are marked*/
+function checkForWin () {
+
+  let countUnmarkedMines = 0;
+  let countHiddenCells = 0;
+
+  for (k = 0; k < board.cells.length; k++) {
+
+    let cellBeingChecked = board.cells[k];
+    
+    if (cellBeingChecked.isMine == true && cellBeingChecked.isMarked != true) {
+      countUnmarkedMines++;
+    }
+
+    if (cellBeingChecked.isMine != true && cellBeingChecked.hidden == true) {
+      countHiddenCells++;
     }
   }
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
-}*/
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
+  if (countUnmarkedMines == 0 || countHiddenCells == 0) {
+    lib.displayMessage('You win!')
+  } else {
+    return;
+  }
+}
 
+/* Function to determine which of the surrounding cells are mines.
+It uses the lib.getSurroundingCells function that was already defined */
 function countSurroundingMines(cell) {
   let surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
   let mineCount = 0;
 
-  for (j = 0; j <= surroundingCells.length - 1; j++) {
+  for (j = 0; j < surroundingCells.length; j++) {
     let individualCell = surroundingCells[j];
     if (individualCell.isMine == true) {
       mineCount++
