6c99be472e9238679263d903a9ef9e1c3023f027
Joe Butler <joefish.b@gmail.com>
2020-05-13 13:03:24 +1200

    Reset board when difficulty button pressed

--- minesweeper.js
+++ minesweeper.js
@@ -1,9 +1,32 @@
-document.addEventListener('DOMContentLoaded', playGame)
+document.addEventListener('DOMContentLoaded', startGame)
 
-var boardSize = null;
+var boardSize = 4;
 var board = {cells: []}
 
-function changeDifficulty() {
+// Function to set up the game
+function startGame() {
+
+  // Create the board
+  document.getElementById("board").innerHTML = "";
+  createBoard();
+  determineSurroundingMines();
+
+  // Events to check for a win scenario
+  document.addEventListener("click", checkForWin);
+  document.addEventListener("contextmenu", checkForWin);
+
+  // Events to reset game with new difficulty
+  document.getElementById('easy').addEventListener('click', resetGame);
+  document.getElementById('medium').addEventListener('click', resetGame);
+  document.getElementById('hard').addEventListener('click', resetGame);
+  
+  // Don't remove this function call: it makes the game work!
+  lib.initBoard()
+}
+
+function resetGame() {
+  board = {cells: []};
+
   if (this.id === 'easy') {
     boardSize = 4;
   } else if (this.id === 'medium') {
@@ -11,8 +34,10 @@ function changeDifficulty() {
   } else if (this.id === 'hard') {
     boardSize = 6;
   }
+  startGame();
 }
 
+// Generate the board
 function createBoard() {
   for (i = 0; i < boardSize; i++) {
     for (j = 0; j < boardSize; j++) {
@@ -31,41 +56,18 @@ function createBoard() {
   }
 }
 
+// Randomly determine if a cell should be a mine
 function generateMine() {
   let chance = Math.random();
-  if (chance <= 0.1) {
+  if (chance <= 0.25) {
     return true;
   } else {
     return false;
   }
 }
 
-// Function to set up the game
-function playGame () {
-  createBoard()
-
-  // For each cell, determine the number of surrounding cells that are mines to display as hints
-  for (i = 0; i < board.cells.length; i++) {
-    let cell = board.cells[i];
-    cell.surroundingMines = countSurroundingMines(cell);
-  }
-
-  // Events to check for a win scenario
-  document.addEventListener("click", checkForWin);
-  document.addEventListener("contextmenu", checkForWin);
-
-  // Check for a board reset
-  document.getElementById('easy').addEventListener('click', changeDifficulty);
-  document.getElementById('medium').addEventListener('click', changeDifficulty);
-  document.getElementById('hard').addEventListener('click', changeDifficulty);
-  
-  // Don't remove this function call: it makes the game work!
-  lib.initBoard()
-}
-
 /* Function to check for a win condition -- if all non-mine cells are revealed.
 For reference, a "cell" here is a non-mine cell and a mine is a mine cell */
-
 function checkForWin () {
 
   let countUnmarkedMines = 0;
@@ -85,6 +87,14 @@ function checkForWin () {
     lib.displayMessage('You win!')
   } else {
     return;
+  }
+}
+
+// For each cell, determine the number of surrounding cells that are mines to display as hints
+function determineSurroundingMines() {
+  for (i = 0; i < board.cells.length; i++) {
+    let cell = board.cells[i];
+    cell.surroundingMines = countSurroundingMines(cell);
   }
 }
 
